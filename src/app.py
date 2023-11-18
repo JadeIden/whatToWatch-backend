a6cd5b1c49f16e9a63a7dce3f2fcf7f86c28bce6
Jade Iden <jiden3@gmail.com>
2023-11-18 14:43:41 -0500

    Unoptimistically: fixed post issue?

--- src/app.py
+++ src/app.py
@@ -40,7 +40,7 @@ def createItem(event, context) -> Union[schema.Item, schema.Error]:
         parsed_body = json.loads(event['body'])
         parsed_item = schema.Item(**parsed_body, itemId=str(uuid.uuid4()))
 
-    _ = items_table.put_item(Item=parsed_item)
+    _ = items_table.put_item(Item=dataclasses.asdict(parsed_item))
 
     return {
         "statusCode": "200",
